47272f30fd85a733f0a4964e7f5fdca1c1c2351f
HimanshuK18 <himanshukhurana18@gmail.com>
2022-12-01 16:04:52 +1100

    new commit

--- constants/constants.ts
+++
@@ -1,4 +0,0 @@
-
-export abstract class Constants {
-    static readonly CAT_API_URL = 'https://api.thecatapi.com/v1/breeds';
-    }
--- dist/constants/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Constants = void 0;
-class Constants {
-}
-exports.Constants = Constants;
-Constants.CAT_API_URL = 'https://api.thecatapi.com/v1/breeds';
-//# sourceMappingURL=constants.js.map
